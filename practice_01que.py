5dd2ff3be38abf4e9f03d9563eda0213634eadd8
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-07-01 10:56:42 +0530

    new file

---
+++ practice_01que.py
@@ -0,0 +1,76 @@
+##----PRACTICE-QUESTION----
+##String method()
+city = "jaipur"
+
+##Slicing
+# print(city[0:3])
+# print(city[0:])
+# print(city[1:5])
+# print(city[::2])
+# print(city[::-1])
+# print(city[-1:])
+
+##1.length()
+# print(len(city))
+# print(len(city)-1)
+# print(len(city)+2)
+# print(city, len(city))
+
+##2.upper()
+# print(city.upper()) #JAIPUR
+
+##3.lower()
+# city_2 = "delhi", "jaipur"
+
+# print(city_2.lower()) #jaipur
+
+##4.title()
+# print("Title is :", city_2.title()) #title-> ex. jaipur
+
+##5.capitalize()
+# print(city_2.capitalize()) #capitalize first charcter
+
+##6.strip()
+# print(city_2.strip())
+
+##7.replace()
+# print(city_2.replace("delhi","sirohi")) #delhi->sirohi
+
+##8.find()
+# print(city_2.find("h")) #3
+
+##9.count()
+# print(city_2.count("e")) #1
+
+##10.split()
+# print(city_2.split(" ")) #'delhi', 'jaipur'
+
+##11.join()
+# print("-".join(city_2)) #delhi-jaipur
+
+##12.isalpha()
+# print(city_2.isalpha()) #True only letters
+
+##13.isdigit()
+# digit = "12345"
+# print(digit.isdigit()) #True only digit
+
+##14.isalnum()
+# text = "Hitesh123"
+# print(text.isalnum()) #True both are letters and digit
+
+##15.isspace()
+# text_1 = " "
+# print(text_1.isspace()) #True check only space
+
+##16.swapcase()
+# name = "HiTeSH"
+# print(name.swapcase()) #Upper to lower and lower to upper
+
+##17.startswith()
+# a = "Hitesh"
+# print(a.startswith("H")) #True start->H
+
+##18.endswith()
+# print(a.endswith("h")) #True end->h
+
