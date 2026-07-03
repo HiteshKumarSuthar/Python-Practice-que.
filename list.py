d2959430545cc85814f46f787c72d0bda1fd4fa0
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-07-03 21:53:13 +0530

    Add new file

---
+++ list.py
@@ -0,0 +1,87 @@
+##LIST -> A list is an ordered and mutable collection that stores multiple items of different data types in a single variable.
+# name = ["Dog", 19, 2.2]
+# print(type(name))
+
+# item = ["Hitesh", "Suresh", "Chirag"]
+
+# for i in item:
+#     print(i)
+
+# item = ["Hitesh", "Suresh", "Chirag"]
+
+# for i in range(2):
+#     print(i)
+
+#Slicing in list
+# list = ["Hitesh", "Suresh", "Chirag"]
+
+
+##LIST-PRACTICE-QUE.
+#Q-1
+# fruits = ["Mango", "Apple", "Banana", "Watermalon", "Litchi"]
+
+# print(fruits)
+
+#Q-2
+# number = [1, 2, 3, 4, 5]
+
+# print(number[0])
+# print(number[2])
+# print(number[4])
+
+#Q-3
+# color = ["Red", "Green", "Blue", "Pink"]
+# color[2] = "Black"
+
+# print(color)
+
+#Q-4
+# city = ["jaipur", "sirohi", "delhi", "mumbai"]
+
+# city.append("Surat")
+# city.insert(2, "Noida")
+
+# print(city)
+
+#Q-5
+# program = ["Python", "Java", "C++", "C", "HTML", "CSS"]
+
+# program.remove("Java")
+# program.pop(0)
+
+# print(program)
+
+#Q-6
+# num = [1, 2, 3, 4, 5, 6, 7, 8]
+
+# num_1 = len(num)
+# print(num_1)
+
+#Q-7
+# name = input("Find the name this list : ")
+
+# list = ["Suresh", "Chirag", "Hitesh"]
+
+# if name in list:
+#     print("This name is exist in this list")
+# else:
+#     print("This name is cannot exist in this list")
+
+
+#Q-8
+
+# number = [40, 30, 48, 89, 87]
+
+# print(max(number))
+# print(min(number))
+
+#Q-9
+# num = [10, 20, 30, 40, 50 , 60, 70, 80, 90, 100]
+
+# print(sum(num))
+
+#Q-10
+# subject = ["python", "java", "c", "c++", "c#"]
+
+# for subjects in subject:
+#     print(subjects)
