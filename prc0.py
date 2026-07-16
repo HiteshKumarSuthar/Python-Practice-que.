91aeb97fa255d518a13857b804a08aef392db7fd
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-07-16 23:08:58 +0530

    updated code

--- prc0.py
+++ prc0.py
@@ -1,30 +1,77 @@
-name = "Hitesh suthar suresh chirag"
+# name = "Hitesh suthar suresh chirag"
 
 
-print(name.split(" "))
+# print(name.split(" "))
 
 
-new = name.split(" ")
+# new = name.split(" ")
 
 
-print(new[0])
-print(len(name))
-print(len(new))
-
-
-#BTS - behind the sence
-#BTW - by the way
-
-
-
-# new_name = name.split(" ")
-# print(new_name[0])
-
+# print(new[0])
 # print(len(name))
+# print(len(new))
+
+
+# #BTS - behind the sence
+# #BTW - by the way
 
 
 
-lst = [1, 3, 4, 5, 8, 0]
-print(lst)
-lst.sort()
-print(lst)
+# # new_name = name.split(" ")
+# # print(new_name[0])
+
+# # print(len(name))
+
+
+
+# lst = [1, 3, 4, 5, 8, 0]
+# print(lst)
+# lst.sort()
+# print(lst)
+
+
+## Dict. practice-que.
+students = {}
+
+number_of_students = int(input("How many students? : "))
+
+for i in range(1, number_of_students+1):
+    name = input("Enter your name: ")
+    
+    while True:
+        
+        age = int(input("Enter your age: "))
+        
+        if age > 0 and age <= 100:
+             break
+        else:
+            print("Invalid age")
+            
+            
+    course = input("Your course: ")
+    city = input("Which is your city? : ")
+
+    ##int -> str
+    format_1 = "Student"
+    number = i
+    student_id = format_1 + str(number)
+
+    students[student_id] = {
+        "name": name,
+        "age": age,
+        "course": course,
+        "city": city
+    }
+
+print(students)
+choice = input("Do you want to see the students?[y/n]: ")
+
+if choice == "y" or choice == "Y":
+    for key, value in students.items():
+        print(key, ":", value)
+        
+elif choice == "n" or choice == "N":
+    print("Good bye...")
+    
+else:
+    print("Invalid choice! Please enter y or n only.")
