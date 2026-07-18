2e2f69cfb2cd27c4ddac2c504b374b513d4f8e8e
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-07-18 19:17:30 +0530

    New code added

--- dict.py
+++ dict.py
@@ -177,47 +177,142 @@
 
 
 ##---Que.---
-students = {}
+# students = {}
 
-number_of_students = int(input("How many students? : "))
+# number_of_students = int(input("How many students? : "))
 
-for i in range(1, number_of_students+1):
-    name = input("Enter your name: ")
+# for i in range(1, number_of_students+1):
+#     name = input("Enter your name: ")
     
-    while True:
+#     while True:
         
-        age = int(input("Enter your age: "))
+#         age = int(input("Enter your age: "))
         
-        if age > 0 and age <= 100:
-             break
+#         if age > 0 and age <= 100:
+#              break
+#         else:
+#             print("Invalid age")
+            
+            
+#     course = input("Your course: ")
+#     city = input("Which is your city? : ")
+
+#     ##int -> str
+#     format_1 = "Student"
+#     number = i
+#     student_id = format_1 + str(number)
+
+#     students[student_id] = {
+#         "name": name,
+#         "age": age,
+#         "course": course,
+#         "city": city
+#     }
+    
+# find_id = input("Enter the student id (eg. Student1, Student2....) : ")
+
+
+# for key,value in students.items():
+#     if find_id == key:
+#         print(value)
+
+
+#         break
+#     else:
+#         print("No student found!")
+        
+        
+        
+##====Question 3: Simple Inventory Management System====
+
+# Create a dictionary to store product details.
+
+# Each product should have:
+
+# Product ID
+# Product Name
+# Price
+# Quantity
+
+# Use a menu-driven program to perform the following operations:
+
+# Add a new product.-
+# Search a product using Product ID.
+# Display all products.
+# Exit the program.
+
+# Conditions:
+
+# Product ID must be unique.
+# Price and Quantity cannot be negative.
+# Store product details using nested dictionaries.
+
+
+# Add = input("Enter a New Product Add : ")
+# Search = input("Search a Product Using ID : ")
+# Exit = input("Exit this Menu : ")
+
+
+
+
+##====PRODUCT INVENTORY PROGRAM====
+products = {}
+
+while True:
+    print("\n**** Product Inventory Management ****")
+    print("1. Add Product")
+    print("2. Delete Product")
+    print("3. Search Product")
+    print("4. Display Products")
+    print("5. Exit")
+    
+    choice = int(input("Enter your choice between (1-4) : "))
+    
+    if choice == 1:
+        while True:
+            product_id = int(input("Enter Product_id (must be unique) : "))
+            
+            if product_id not in products:
+                break
+            else:
+                print("Invalid id")
+                
+        product_name = input("Enter product name : ")
+        price = int(input("Enter product price (INR) : "))
+        quantity = int(input("Enter product quantity : "))
+        
+        products[product_id] = {
+            "Product Name": product_name,
+            "Price": price,
+            "Quantity": quantity
+        }
+        
+        print("\nProduct added successfully!")
+    
+    elif choice == 2:
+        input_product_id = int(input("Enter product id : "))
+        
+        if input_product_id in products:
+            del products[input_product_id]
+            print("Product deleted...")
         else:
-            print("Invalid age")
-            
-            
-    course = input("Your course: ")
-    city = input("Which is your city? : ")
-
-    ##int -> str
-    format_1 = "Student"
-    number = i
-    student_id = format_1 + str(number)
-
-    students[student_id] = {
-        "name": name,
-        "age": age,
-        "course": course,
-        "city": city
-    }
-    
-find_id = input("Enter the student id (eg. Student1, Student2....) : ")
-
-
-for key,value in students.items():
-    if find_id == key:
-        print(value)
-
-
-    #     break
-    # else:
-    #     print("No student found!")
+            print("Product not found!")
         
+    elif choice == 3:
+        search_product_id = int(input("Enter the product id : "))
+        
+        for productid, productdetails in products.items():
+            if search_product_id == productid:
+                print(productid, ":", productdetails)
+            
+            
+    elif choice == 4:
+       for productid, product_details in products.items():
+           print(productid, ":", product_details)
+           
+    elif choice == 5:
+        print("Thank you for using this product inventory")
+    
+    else:
+        print("Invalid choice")
+    
