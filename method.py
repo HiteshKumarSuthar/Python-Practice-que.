1834223895e4f02d2e90bc3d32e62b19a31fef70
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-07-21 18:59:51 +0530

    code add

--- method.py
+++ method.py
@@ -1,12 +1,63 @@
-def display(text, times):
+# def display(text, times):
     
-    if text == "I love you" or text == "Sorry":
-        for i in range(1, times+1):
-            print(i,".",text)
+#     if text == "I love you" or text == "Sorry":
+#         for i in range(1, times+1):
+#             print(i,".",text)
             
-    else:
-        print(f"{text} is not accepted!")
+#     else:
+#         print(f"{text} is not accepted!")
         
 
-display(input("Enter your message: "), int(input("How many times? :  ")))
+# display(input("Enter your message: "), int(input("How many times? :  ")))
+
+
+#Q-1
+# def greet():
+#     print("Welcome to Python Programming")
+
+# greet()
+# greet()
+# greet()
+# greet()
+# greet()
+
+#Q-2
+# def square():
+#     print(13 ** 2)
+    
+# square()
+
+#Q-3
+# Create two functions even() and odd(). even() should print all even numbers from *1 to 20, and odd() should print all odd numbers from **1 to 20*.
+
+
+while True:
+    
+    def even():
+        for i in range(1, 21):
+            if i % 2 == 0:
+                print(i)
+
+
+    def odd():
+        for i in range(1, 21):
+            if i % 2 != 0:
+                print(i)
+                
+    
+    print("1. Even number")
+    print("2. Odd number")
+    print("3. Exit")
+    
+    choice = int(input("Enter your Choice : "))
+    
+    if choice == 1:
+        even()
+    elif choice == 2:
+        odd()
+    elif choice == 3:
+        print("Your are left this program!")
+        break
+    else:
+        print("Invalid choice....")
 
