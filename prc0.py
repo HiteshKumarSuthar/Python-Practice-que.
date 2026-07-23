3dde7e08cec78b40c71566bc0066d773e0301aeb
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-07-23 22:45:24 +0530

    Code updated

--- prc0.py
+++ prc0.py
@@ -31,47 +31,47 @@
 
 
 ## Dict. practice-que.
-students = {}
+# students = {}
 
-number_of_students = int(input("How many students? : "))
+# number_of_students = int(input("How many students? : "))
 
-for i in range(1, number_of_students+1):
-    name = input("Enter your name: ")
+# for i in range(1, number_of_students+1):
+#     name = input("Enter your name: ")
     
-    while True:
+#     while True:
         
-        age = int(input("Enter your age: "))
+#         age = int(input("Enter your age: "))
         
-        if age > 0 and age <= 100:
-             break
-        else:
-            print("Invalid age")
+#         if age > 0 and age <= 100:
+#              break
+#         else:
+#             print("Invalid age")
             
             
-    course = input("Your course: ")
-    city = input("Which is your city? : ")
+#     course = input("Your course: ")
+#     city = input("Which is your city? : ")
 
-    ##int -> str
-    format_1 = "Student"
-    number = i
-    student_id = format_1 + str(number)
+#     ##int -> str
+#     format_1 = "Student"
+#     number = i
+#     student_id = format_1 + str(number)
 
-    students[student_id] = {
-        "name": name,
-        "age": age,
-        "course": course,
-        "city": city
-    }
+#     students[student_id] = {
+#         "name": name,
+#         "age": age,
+#         "course": course,
+#         "city": city
+#     }
 
-print(students)
-choice = input("Do you want to see the students?[y/n]: ")
+# print(students)
+# choice = input("Do you want to see the students?[y/n]: ")
 
-if choice == "y" or choice == "Y":
-    for key, value in students.items():
-        print(key, ":", value)
+# if choice == "y" or choice == "Y":
+#     for key, value in students.items():
+#         print(key, ":", value)
         
-elif choice == "n" or choice == "N":
-    print("Good bye...")
+# elif choice == "n" or choice == "N":
+#     print("Good bye...")
     
-else:
-    print("Invalid choice! Please enter y or n only.")
+# else:
+#     print("Invalid choice! Please enter y or n only.")
