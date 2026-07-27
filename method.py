6de10847facdb2c865276f696a114f2a86a0b9f5
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-07-27 19:01:01 +0530

    code

--- method.py
+++ method.py
@@ -1,375 +1,375 @@
-# ##===METHODS(Function)===
+##===METHODS(Function)===
 
-# def display(text, times):
+def display(text, times):
     
-#     if text == "Welcome to python program" or text == "Exit this program":
-#         for i in range(1, times+1):
-#             print(i,".",text)
+    if text == "Welcome to python program" or text == "Exit this program":
+        for i in range(1, times+1):
+            print(i,".",text)
             
-#     else:
-#         print(f"{text} is not accepted!")
+    else:
+        print(f"{text} is not accepted!")
         
 
-# display(input("Enter your message: "), int(input("How many times? :  ")))
+display(input("Enter your message: "), int(input("How many times? :  ")))
 
 
 
-# ##===Q-1===
-# def greet():
-#     print("Welcome to Python Programming")
+##===Q-1===
+def greet():
+    print("Welcome to Python Programming")
 
-# greet()
-# greet()
-# greet()
-# greet()
-# greet()
+greet()
+greet()
+greet()
+greet()
+greet()
 
 
 
-# ##===Q-2===
-# def square():
-#     print(13 ** 2)
+##===Q-2===
+def square():
+    print(13 ** 2)
     
-# square()
+square()
 
 
 
-# ##===Q-3===
-# # This Menu-driven Program
+##===Q-3===
+# This Menu-driven Program
 
-# while True:
+while True:
     
-#     def even():
-#         for i in range(1, 21):
-#             if i % 2 == 0:
-#                 print(i)
+    def even():
+        for i in range(1, 21):
+            if i % 2 == 0:
+                print(i)
 
 
-#     def odd():
-#         for i in range(1, 21):
-#             if i % 2 != 0:
-#                 print(i)
+    def odd():
+        for i in range(1, 21):
+            if i % 2 != 0:
+                print(i)
                 
     
-#     print("1. Even number")
-#     print("2. Odd number")
-#     print("3. Exit")
+    print("1. Even number")
+    print("2. Odd number")
+    print("3. Exit")
     
-#     choice = int(input("Enter your Choice : "))
+    choice = int(input("Enter your Choice : "))
     
-#     if choice == 1:
-#         even()
-#     elif choice == 2:
-#         odd()
-#     elif choice == 3:
-#         print("Your are left this program!")
-#         break
-#     else:
-#         print("Invalid choice....")
+    if choice == 1:
+        even()
+    elif choice == 2:
+        odd()
+    elif choice == 3:
+        print("Your are left this program!")
+        break
+    else:
+        print("Invalid choice....")
 
 
 
-# ##===Q-4===
-# # Create a function calculator() that displays the following menu:
+##===Q-4===
+# Create a function calculator() that displays the following menu:
 
    
-# #    1. Addition
-# #    2. Subtraction
-# #    3. Multiplication
-# #    4. Division
+#    1. Addition
+#    2. Subtraction
+#    3. Multiplication
+#    4. Division
    
 
-# #    Then call the function.
+#    Then call the function.
 
-# def calculator():
-#     while True:
-#         print("1. Addition")
-#         print("2. Substraction")
-#         print("3. Multiplication")
-#         print("4. Division")
-#         print("5. Exit")
+def calculator():
+    while True:
+        print("1. Addition")
+        print("2. Substraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Exit")
         
-#         cal = int(input("Enter the number and calculate numbers : "))
+        cal = int(input("Enter the number and calculate numbers : "))
         
-#         if cal == 1:
-#             number_1 = int(input("Enter the number : "))
-#             number_2 = int(input("Enter the number : "))
-#             print(number_1 + number_2)
+        if cal == 1:
+            number_1 = int(input("Enter the number : "))
+            number_2 = int(input("Enter the number : "))
+            print(number_1 + number_2)
             
-#         elif cal == 2:
-#             number_1 = int(input("Enter the number : "))
-#             number_2 = int(input("Enter the number : "))
-#             print(number_1 - number_2)
+        elif cal == 2:
+            number_1 = int(input("Enter the number : "))
+            number_2 = int(input("Enter the number : "))
+            print(number_1 - number_2)
             
-#         elif cal == 3:
-#             number_1 = int(input("Enter the number : "))
-#             number_2 = int(input("Enter the number : "))
-#             print(number_1 * number_2)
+        elif cal == 3:
+            number_1 = int(input("Enter the number : "))
+            number_2 = int(input("Enter the number : "))
+            print(number_1 * number_2)
             
-#         elif cal == 4:
-#             number_1 = int(input("Enter the number : "))
-#             number_2 = int(input("Enter the number : "))
-#             print(number_1 / number_2)
+        elif cal == 4:
+            number_1 = int(input("Enter the number : "))
+            number_2 = int(input("Enter the number : "))
+            print(number_1 / number_2)
             
-#         elif cal == 5:
-#             print("Exit this Program")
-#             break
+        elif cal == 5:
+            print("Exit this Program")
+            break
         
-#         else:
-#             print("Invalid number..Please Try Again....")
+        else:
+            print("Invalid number..Please Try Again....")
 
 
-# calculator()
+calculator()
 
 
 
-# ##===Q-5===
-# def calculator():
-#     while True:
-#         print("1. Addition")
-#         print("2. Substraction")
-#         print("3. Multiplication")
-#         print("4. Division")
-#         print("5. Exit")
+##===Q-5===
+def calculator():
+    while True:
+        print("1. Addition")
+        print("2. Substraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Exit")
         
-#         number_1 = float(input("Enter first number : "))
-#         number_2 = float(input("Enter second number : "))
+        number_1 = float(input("Enter first number : "))
+        number_2 = float(input("Enter second number : "))
         
-#         def addition(num1, num2):
-#             return num1 + num2
+        def addition(num1, num2):
+            return num1 + num2
         
-#         def subtraction(num1, num2):
-#             return num1 - num2
+        def subtraction(num1, num2):
+            return num1 - num2
         
-#         def multiplication(num1, num2):
-#             return num1 * num2
+        def multiplication(num1, num2):
+            return num1 * num2
             
-#         def division(num1, num2):
-#             if num2 == 0:
-#                 return "Cannot divide by zero."
-#             else:
-#                 return num1 / num2
+        def division(num1, num2):
+            if num2 == 0:
+                return "Cannot divide by zero."
+            else:
+                return num1 / num2
                 
-#         cal = int(input("Enter your choice : "))
+        cal = int(input("Enter your choice : "))
         
-#         if cal == 1:
-#             result = addition(number_1, number_2)
-#             print(result)
+        if cal == 1:
+            result = addition(number_1, number_2)
+            print(result)
            
-#         elif cal == 2:
-#            result = subtraction(number_1, number_2)
-#            print(result)
+        elif cal == 2:
+           result = subtraction(number_1, number_2)
+           print(result)
            
-#         elif cal == 3:
-#             result = multiplication(number_1, number_2)
-#             print(result)
+        elif cal == 3:
+            result = multiplication(number_1, number_2)
+            print(result)
             
-#         elif cal == 4:
-#             result = division(number_1, number_2)
-#             print(result)
+        elif cal == 4:
+            result = division(number_1, number_2)
+            print(result)
             
-#         elif cal == 5:
-#             print("Exiting...")
-#             break
-#         else:
-#             print("Invalid number..Please Try Again....")
+        elif cal == 5:
+            print("Exiting...")
+            break
+        else:
+            print("Invalid number..Please Try Again....")
 
 
-# calculator()
+calculator()
 
 
 
-# ##===Q-6===
-# def student():
-#     print("Name",":","Hitesh")
+##===Q-6===
+def student():
+    print("Name",":","Hitesh")
 
-# def course():
-#     print("Course",":","Python")
+def course():
+    print("Course",":","Python")
 
-# def college():
-#     print("College",":","Poornima University")
+def college():
+    print("College",":","Poornima University")
     
-# student()
-# course()
-# college()
+student()
+course()
+college()
 
 
 
-#-1-
-# def table(num):
-#     for i in range(1, 11):
-#         print(i * num)
+# -1-
+def table(num):
+    for i in range(1, 11):
+        print(i * num)
         
-# table(int(input("Enter the number : ")))
+table(int(input("Enter the number : ")))
 
 
-#-2-
-# def table(num):
-#     i = 1
-#     while i <= 10:
-#         print(i * num)
-#         i += 1
+# -2-
+def table(num):
+    i = 1
+    while i <= 10:
+        print(i * num)
+        i += 1
         
-# table(int(input("Enter the number : ")))
+table(int(input("Enter the number : ")))
 
 
 
-##=== 1. ATM machine in python (Without Verification ATM machine program)===
+#=== 1. ATM machine in python (Without Verification ATM machine program)===
 
-# def atm():
+def atm():
     
-#     balance = 1000.0
+    balance = 1000.0
     
-#     pin = int(input("Enter your pin : "))
+    pin = int(input("Enter your pin : "))
     
-#     while True:
+    while True:
         
-#         print("1. Show Balance")
-#         print("2. Deposit Money")
-#         print("3. Withdraw Money")
-#         print("4. Exit")
+        print("1. Show Balance")
+        print("2. Deposit Money")
+        print("3. Withdraw Money")
+        print("4. Exit")
         
-#         n = int(input("Enter the n : "))
+        n = int(input("Enter the n : "))
         
-#         if n == 1:
-#             # acc_balance = float(input("Enter the balance : "))
-#             # balance += acc_balance
-#             print("Welcome to SBI Bank ATM","\nYour Account Balance :", balance)
+        if n == 1:
+            # acc_balance = float(input("Enter the balance : "))
+            # balance += acc_balance
+            print("Welcome to SBI Bank ATM","\nYour Account Balance :", balance)
             
-#         elif n == 2:
-#             acc_balance = float(input("Enter the deposit balance : "))
-#             balance += acc_balance
-#             print("Dear Customer, A/c XXXXXX1234 debited for Rs", balance)
+        elif n == 2:
+            acc_balance = float(input("Enter the deposit balance : "))
+            balance += acc_balance
+            print("Dear Customer, A/c XXXXXX1234 debited for Rs", balance)
         
-#         elif n == 3:
-#             acc_balance = float(input("Enter the withdraw money : "))
-#             balance -= acc_balance
-#             print("Current Balance :", balance)
+        elif n == 3:
+            acc_balance = float(input("Enter the withdraw money : "))
+            balance -= acc_balance
+            print("Current Balance :", balance)
 
-#         elif n == 4:
-#             print("Transaction has completed...\nThank you visit again !!")
-#             break
+        elif n == 4:
+            print("Transaction has completed...\nThank you visit again !!")
+            break
         
-#         else:
-#             print("Incorrect value")
+        else:
+            print("Incorrect value")
             
             
-# atm()
+atm()
 
 
 
-##===2. ATM machine in python (Verification system integration)===
+#===2. ATM machine in python (Verification system integration)===
 
-# def ATM():
+def ATM():
     
-#     balance = 1000.0
+    balance = 1000.0
     
-#     MPIN = int(input("Enter your MPIN : "))
+    MPIN = int(input("Enter your MPIN : "))
     
-#     username = input("Enter your username: ")
-#     password = int(input("Create your password: "))
+    username = input("Enter your username: ")
+    password = int(input("Create your password: "))
     
-#     user = {}
+    user = {}
     
-#     user[username] = {
-#         "Password": password
-#     }
+    user[username] = {
+        "Password": password
+    }
     
-#     print(user)
+    print(user)
     
-#     choice = input("Do you want to login?(Y/N): ")
+    choice = input("Do you want to login?(Y/N): ")
     
-#     if choice == "y" or choice == "Y":
-#         login_username = input("Username: ")
-#         login_password = int(input("Password: "))
+    if choice == "y" or choice == "Y":
+        login_username = input("Username: ")
+        login_password = int(input("Password: "))
         
-#         for key, value in user.items():
-#             if login_username == username and login_password == password:
-#                 print("Logged in successfully, WELCOME")
-#                 while True:
+        for key, value in user.items():
+            if login_username == username and login_password == password:
+                print("Logged in successfully, WELCOME")
+                while True:
                                     
-#                             print("1. Show Balance")
-#                             print("2. Deposit Money")
-#                             print("3. Withdraw Money")
-#                             print("4. Exit")
+                            print("1. Show Balance")
+                            print("2. Deposit Money")
+                            print("3. Withdraw Money")
+                            print("4. Exit")
                                     
-#                             option = int(input("Choose a option any one : "))
+                            option = int(input("Choose a option any one : "))
                                     
-#                             if option == 1:
-#                             # acc_balance = float(input("Enter the balance : "))
-#                             # balance += acc_balance
-#                                 print("Welcome to SBI Bank ATM","\nYour Account Balance :", balance)
+                            if option == 1:
+                            # acc_balance = float(input("Enter the balance : "))
+                            # balance += acc_balance
+                                print("Welcome to SBI Bank ATM","\nYour Account Balance :", balance)
                                         
-#                             elif option == 2:
-#                                 acc_balance = float(input("Enter the deposit balance : "))
-#                                 balance += acc_balance
-#                                 print("Dear Customer, A/c XXXXXX1234 debited for Rs", balance)
+                            elif option == 2:
+                                acc_balance = float(input("Enter the deposit balance : "))
+                                balance += acc_balance
+                                print("Dear Customer, A/c XXXXXX1234 debited for Rs", balance)
                                     
-#                             elif option == 3:
-#                                 acc_balance = float(input("Enter the withdraw money : "))
-#                                 balance -= acc_balance
-#                                 print("Current Balance :", balance)
+                            elif option == 3:
+                                acc_balance = float(input("Enter the withdraw money : "))
+                                balance -= acc_balance
+                                print("Current Balance :", balance)
                             
-#                             elif option == 4:
-#                                 print("Transaction has completed...\nThank you visit again !!")
-#                                 break
+                            elif option == 4:
+                                print("Transaction has completed...\nThank you visit again !!")
+                                break
                                     
-#                             else:
-#                                 print("Incorrect value")  
-#             else:
-#                 print("Invalid username or password!")
+                            else:
+                                print("Incorrect value")  
+            else:
+                print("Invalid username or password!")
                 
     
                                 
-# ATM()
+ATM()
 
 
 
-# username = input("Enter your username: ")
-# password = int(input("Create your password: "))
+username = input("Enter your username: ")
+password = int(input("Create your password: "))
 
-# user = {}
+user = {}
 
-# user[username] = {
-#     "Password": password
-# }
+user[username] = {
+    "Password": password
+}
 
-# choice = input("Do you want to login?(Y/y) | (N/n): ")
+choice = input("Do you want to login?(Y/y) | (N/n): ")
 
-# if choice == "y" or choice == "Y":
-#     login_username = input("Username: ")
-#     login_password = int(input("Password: "))
+if choice == "y" or choice == "Y":
+    login_username = input("Username: ")
+    login_password = int(input("Password: "))
     
-#     for key, value in user.items():
-#         if login_username == username and login_password == password:
-#             print("Logged in successfully, WELCOME")
+    for key, value in user.items():
+        if login_username == username and login_password == password:
+            print("Logged in successfully, WELCOME")
             
-#         else:
-#             print("Invalid username or password!")
+        else:
+            print("Invalid username or password!")
             
-# elif choice == "n" or choice == "N": 
-#     print("Thank you for using ATM!")
-#     print("Exiting...")
+elif choice == "n" or choice == "N": 
+    print("Thank you for using ATM!")
+    print("Exiting...")
     
-# else:
-#     print("Please choose 'Y' or 'N'.")
+else:
+    print("Please choose 'Y' or 'N'.")
 
 
 
 
-# banks = ["SBI", "AXIS", "HDFC", "ICICI", "PNB", "CANARA", "RMGB"]
+banks = ["SBI", "AXIS", "HDFC", "ICICI", "PNB", "CANARA", "RMGB"]
 
-# input_bank = input("Enter your bank: ")
-# new_bank = input_bank.upper()
+input_bank = input("Enter your bank: ")
+new_bank = input_bank.upper()
 
-# for each in banks:
-#     if new_bank == each:
-#         print(f"WELCOME TO {new_bank}!")
+for each in banks:
+    if new_bank == each:
+        print(f"WELCOME TO {new_bank}!")
         
-#     else:
-#         print("Bank not found.")
-#         print("\nAvailable banks: ")
-#         new_banks = ", ".join(banks)
-#         print(new_banks)
-#         break
+    else:
+        print("Bank not found.")
+        print("\nAvailable banks: ")
+        new_banks = ", ".join(banks)
+        print(new_banks)
+        break
     
