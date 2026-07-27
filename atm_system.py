f5ad58d7e1f85920b9bbddf1b1d0cd5c5923c631
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-07-28 00:11:48 +0530

    updated

--- atm_system.py
+++ atm_system.py
@@ -1,47 +1,47 @@
 ##===Program(1) ATM machine in python (Without Verification ATM machine program)===
 
-# def ATM():
+def ATM():
     
-#     balance = 1000.0
+    balance = 1000.0
     
-#     pin = int(input("Enter your pin : "))
+    pin = int(input("Enter your pin : "))
     
-#     while True:
+    while True:
         
-#         print("1. Show Balance")
-#         print("2. Credited Money")
-#         print("3. Withdraw Money")
-#         print("4. Exit")
+        print("1. Show Balance")
+        print("2. Credited Money")
+        print("3. Withdraw Money")
+        print("4. Exit")
         
-#         n = int(input("Enter the n : "))
+        n = int(input("Enter the n : "))
         
-#         if n == 1:
-#             # acc_balance = float(input("Enter the balance : "))
-#             # balance += acc_balance
-#             print("Welcome to SBI Bank ATM","\nYour Account Balance :", balance)
+        if n == 1:
+            # acc_balance = float(input("Enter the balance : "))
+            # balance += acc_balance
+            print("Welcome to SBI Bank ATM","\nYour Account Balance :", balance)
             
-#         elif n == 2:
-#             acc_balance = float(input("Enter the credited balance : "))
-#             balance += acc_balance
-#             print("Dear Customer, A/c XXXXXX1234 credited for Rs", balance)
+        elif n == 2:
+            acc_balance = float(input("Enter the credited balance : "))
+            balance += acc_balance
+            print("Dear Customer, A/c XXXXXX1234 credited for Rs", balance)
         
-#         elif n == 3:
-#             acc_balance = float(input("Enter the withdraw money : "))
-#             if acc_balance <= balance:
-#                 balance -= acc_balance
-#                 print("Current Balance :", balance)
-#             else:
-#                 print("Insufficient Funds")
+        elif n == 3:
+            acc_balance = float(input("Enter the withdraw money : "))
+            if acc_balance <= balance:
+                balance -= acc_balance
+                print("Current Balance :", balance)
+            else:
+                print("Insufficient Funds")
 
-#         elif n == 4:
-#             print("Transaction has completed...\nThank you visit again !!")
-#             break
+        elif n == 4:
+            print("Transaction has completed...\nThank you visit again !!")
+            break
         
-#         else:
-#             print("Incorrect value")
+        else:
+            print("Incorrect value")
             
             
-# ATM()
+ATM()
 
 
 
