07b08f04456ff3d024fb8f287e55c037497ba79c
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-07-28 23:36:58 +0530

    Upadate

--- scope.py
+++ scope.py
@@ -14,6 +14,7 @@
 
 # -------------------------------------------------------------------------
 
+
 # 3. Global Scope
 # Accessible anywhere in the code.
 # number = 12345
@@ -25,6 +26,7 @@
 
 # -------------------------------------------------------------------------
 
+
 # 4. Built-in Scope
 # numbers = [1,2,3,4,5]
 # name = "Hitesh Suthar"
@@ -32,6 +34,7 @@
 # print(numbers)
 
 # ------------------------------------------------------------------------
+
 
 # 2. Enclosing Scope
 # y = 300 # Globale Scope
@@ -47,6 +50,7 @@
 # outside()
 # print(y) # Output: 300
 
+# ------------------------------------------------------------------------
 
 
 # Question 1: Highest Even Number
@@ -68,17 +72,71 @@
 # # Output: No Even Number
 
 
-def highest_even(lst):
-    lst = [5, 2, 9, 12, 7, 18]
+# def highest_even(lst):
+#     lst = [5, 2, 9, 12, 7, 18]
     
-    highest_even_num = None
-    for i in lst:
-        if i % 2 == 0:
-            if highest_even_num is None or i > highest_even_num:
-                highest_even_num = i
-    return highest_even_num
+#     highest_even_num = None
+#     for i in lst:
+#         if i % 2 == 0:
+#             if highest_even_num is None or i > highest_even_num:
+#                 highest_even_num = i
+#     return highest_even_num
 
 
-lst = [5, 2, 9, 12, 7, 18]
-result = highest_even(lst)
-print("Highest Even Number :", result)
+# lst = [5, 2, 9, 12, 7, 18]
+# result = highest_even(lst)
+# print("Highest Even Number :", result)
+
+# ------------------------------------------------------------------------
+
+
+# def highest_even(numbers):
+    
+#     even_numbers = []
+#     for each in numbers:
+#         if each % 2 == 0:
+#             even_numbers.append(each)
+            
+#     even_numbers.sort(reverse=True)
+#     return even_numbers[0]
+    
+# result = highest_even(list(map (int, input("Enter numbers with space: ").split())))
+
+# print("Highest Even Number: ", result)
+
+
+# ------------------------------------------------------------------------
+
+
+# Question 2: Word Frequency Counter
+
+# Ek function word_frequency(sentence, word) banao.
+
+# Rules:
+
+# sentence aur word parameters honge.
+# Function bataye ki word sentence me kitni baar aaya hai.
+# Matching case-insensitive honi chahiye.
+# Punctuation ignore karna zaroori nahi hai.
+
+# Example:
+
+# word_frequency("Python is easy. python is powerful. PYTHON rocks", "python")
+# # Output: 3
+
+
+def word_frequency(sentence, word):
+    words = sentence.lower().replace(".", "").split()
+
+    count = 0
+
+    for i in words:
+        if i == word.lower():
+            count += 1
+
+    return count
+
+result = word_frequency("Python is easy. python is powerful. PYTHON rocks", "python")
+
+print(result)
+
