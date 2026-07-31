a32d398f2215fbedc9feae5ff11909066b7e9482
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-07-31 23:25:22 +0530

    add

--- file_handling.py
+++ file_handling.py
@@ -1,4 +1,5 @@
 ##===File Handling===
+## File Read & Write and print vowels and count a vowels in this file.
 
 with open("movies.txt", "w+") as file:
     file.write("Learning Python is enjoyable because it helps solve real problems through simple code. Every practice session improves logical thinking, debugging skills, and confidence. Reading files, writing data, handling exceptions, and creating projects build practical experience. Consistent effort each day leads to stronger programming knowledge and better career opportunities.")
