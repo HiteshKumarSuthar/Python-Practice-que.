83656b91b8920ee8e39d5c9ffaeb09dae6a8cd89
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-08-04 21:41:36 +0530

    New file

---
+++ main.py
@@ -0,0 +1,22 @@
+##===Main File===
+
+# import calculator as cal
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+
+# print("addition = ", cal.addition(num1, num2))
+# print("subtraction = ", cal.subtraction(num1, num2))
+# print("multiply = ", cal.multiply(num1, num2))
+
+
+
+import math_util as m
+
+number = int(input("Enter the first number: "))
+
+
+
+print("Squre :", m.squre(number))
+print("Cube :", m.cube(number))
+print("Even :", m.is_even(number))
+
