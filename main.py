e3d863f7cc7e3a363cb71fd861750087251f0423
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-08-05 18:30:30 +0530

    code add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 ##===Main File===
 
+##Q-1
 # import calculator as cal
 # num1 = int(input("Enter the first number: "))
 # num2 = int(input("Enter the second number: "))
@@ -9,14 +10,21 @@
 # print("multiply = ", cal.multiply(num1, num2))
 
 
+##Q-2
+# import math_util as m
 
-import math_util as m
-
-number = int(input("Enter the first number: "))
+# number = int(input("Enter the first number: "))
 
 
+# print("Squre :", m.squre(number))
+# print("Cube :", m.cube(number))
+# print("Even :", m.is_even(number))
 
-print("Squre :", m.squre(number))
-print("Cube :", m.cube(number))
-print("Even :", m.is_even(number))
+
+##Q-3
+import calculator as cel
+
+Celsius = int(input("Enter temp in Celsius: "))
+
+print("Fahrenheit :", cel.celsius(Celsius))
 
