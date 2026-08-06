2a650aefd12cedd7c3af09ce5ef98cb7065e1db3
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-08-06 10:28:49 +0530

    code update

--- math_util.py
+++ math_util.py
@@ -1,13 +1,13 @@
 ##===Module File===
 
-def squre(a):
-    return a ** 2
-def cube(a):
-    return a ** 3
+# def squre(a):
+#     return a ** 2
+# def cube(a):
+#     return a ** 3
 
-def is_even(num):
-    if num % 2 == 0:
-        return True
-    else:
-        return False
+# def is_even(num):
+#     if num % 2 == 0:
+#         return True
+#     else:
+#         return False
         
