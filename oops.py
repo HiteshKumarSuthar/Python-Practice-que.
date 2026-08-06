98d2aec1540596580cb3c85b76a43c17220cb951
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-08-06 09:41:00 +0530

    code updated

--- oops.py
+++ oops.py
@@ -16,14 +16,51 @@
 
 ##Q-2
 
-class car:
-    def __init__(self, brand, model):
-        self.car_brand = brand
-        self.car_model = model
+# class car:
+#     def __init__(self, brand, model):
+#         self.car_brand = brand
+#         self.car_model = model
+
+#     def display(self):
+#         print("Brand :", self.car_brand)
+#         print("Model :", self.car_model)
+
+
+# cars = car("Hyundai", "i20")
+# cars.display()
+
+
+##Q-3
+
+# class faculty:
+#     def __init__(self, name, course):
+#         self.Name = name
+#         self.Course = course
+
+#     def display(self):
+#         print("Name :", self.Name)
+#         print("Course :", self.Course)
+        
+# fac = faculty("Hitesh", "Python")
+# fac.display()
+
+
+##Q-4
+class Students:
+    def __init__(self, name, address):
+        self.Name = name
+        self.Address = address
 
     def display(self):
-        print("Brand :", self.car_brand)
-        print("Model :", self.car_model)
+        print(self.Name, "-", self.Address)
+        
+students = []
+students.append(Students("NAME", "ADDRESS"))
+students.append(Students("Hitesh", "Jaipur"))
+students.append(Students("Suresh", "Delhi"))
+students.append(Students("Chirag", "Noida"))
+students.append(Students("Rohan", "Pune"))
 
-cars = car("Hyundai", "i20")
-cars.display()
+for student in students:
+    student.display()
+    
