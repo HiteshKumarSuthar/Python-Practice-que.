2c7039382778dbf469fdd231271a64026aaf3d76
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-08-08 19:58:45 +0530

    file name change

---
+++ module_1.py
@@ -0,0 +1,13 @@
+##===Module File===
+
+# def squre(a):
+#     return a ** 2
+# def cube(a):
+#     return a ** 3
+
+# def is_even(num):
+#     if num % 2 == 0:
+#         return True
+#     else:
+#         return False
+        
