6939ec6cd07a295b6294fb2744defa408e9c0280
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-08-08 19:58:57 +0530

    file name change

---
+++ module_2.py
@@ -0,0 +1,30 @@
+##===Main File===
+
+##Q-1
+# import calculator as cal
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+
+# print("addition = ", cal.addition(num1, num2))
+# print("subtraction = ", cal.subtraction(num1, num2))
+# print("multiply = ", cal.multiply(num1, num2))
+
+
+##Q-2
+# import math_util as m
+
+# number = int(input("Enter the first number: "))
+
+
+# print("Squre :", m.squre(number))
+# print("Cube :", m.cube(number))
+# print("Even :", m.is_even(number))
+
+
+##Q-3
+import calculator as cel
+
+Celsius = int(input("Enter temp in Celsius: "))
+
+print("Fahrenheit :", cel.celsius(Celsius))
+
