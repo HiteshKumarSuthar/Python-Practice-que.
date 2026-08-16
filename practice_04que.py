aada1e6ce010bb0f6e2d149c71bf9eeb0f45c2f1
HiteshKumarSuthar <hiteshstr11@gmail.com>
2026-08-16 22:34:14 +0530

    new code push

---
+++ practice_04que.py
@@ -0,0 +1,33 @@
+##===Class python test===
+##Q-1
+# arr = [10, 20, 30, 5, 6, 7]
+
+# print("largest number is (using indexing) :", arr[2])
+# print("largest number is (using max method) :", max(arr))
+
+
+##Q-2
+# num_1 = int(input("Enter the number : "))
+# num_2 = int(input("Enter the number : "))
+
+
+# print(num_1 * num_2)
+
+
+##Q-3
+# arr = [1, 2, 3, 4, 5]
+
+# s = sum(arr)
+# print(s)
+
+
+##Q-4
+# arr = [1, 2, 3, 4, 5, 6]
+
+# print(arr[::-1])
+
+
+# ##Q-5
+# str = "hello"
+
+# print(str[::-1])
